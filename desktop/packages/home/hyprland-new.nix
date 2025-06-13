85c4dd7d792e917bb32534cfcdb2f755f62c188b
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-06-13 22:58:40 +0900

    Moving KDE polkit

--- desktop/packages/home/hyprland-new.nix
+++ desktop/packages/home/hyprland-new.nix
@@ -343,23 +343,6 @@ in
       wallpaper = [ (", " + pic) ];
     };
   };
-  # Run polkit service
-  # I use KDE polkit because it allows selecting a user
-  systemd.user.services.polkit = {
-    Unit.Description = "KDE polkit";
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-      Wants = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
   services.hypridle = {
     enable = true;
     settings = {
--- desktop/packages/home/kde.nix
+++ desktop/packages/home/kde.nix
@@ -1,5 +1,27 @@
-{ unstable, config, ... }:
 {
+  pkgs,
+  unstable,
+  config,
+  ...
+}:
+{
+  # Run polkit service
+  # I use KDE polkit because it allows selecting a user
+  systemd.user.services.polkit = {
+    Unit.Description = "KDE polkit";
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+      Wants = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
   xdg.configFile."kdeglobals".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Sync/LinuxConfig/kde/kdeglobals";
   xdg.configFile."dolphinrc".source =
