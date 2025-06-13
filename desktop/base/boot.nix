6523d3a82d8eccc57c92ab277e82525992af0990
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-06-13 21:35:19 +0900

    Automatic resolution setting

--- desktop/base/boot.nix
+++ desktop/base/boot.nix
@@ -1,9 +1,14 @@
-{ inputs, ... }:{
+{ inputs, device, ... }:
+{
   imports = [ inputs.grub2-themes.nixosModules.default ];
   boot.loader.grub2-theme = {
     enable = true;
     theme = "stylish";
     footer = true;
-    customResolution = "1920x1080";  # Optional: Set a custom resolution
+    customResolution =
+      let
+        mon = (builtins.elemAt device.monitors 0);
+      in
+      "${builtins.toString mon.x}x${builtins.toString mon.y}";
   };
 }
