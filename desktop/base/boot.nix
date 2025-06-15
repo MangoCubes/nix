df43aaae593727e690f4428181c6f421e03b832f
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-06-15 23:54:05 +0900

    Boot themes

--- desktop/base/boot.nix
+++ desktop/base/boot.nix
@@ -1,6 +1,38 @@
-{ inputs, device, ... }:
 {
-  imports = [ inputs.grub2-themes.nixosModules.default ];
+  inputs,
+  device,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    inputs.grub2-themes.nixosModules.default
+    inputs.mikuboot.nixosModules.default
+  ];
+  boot.plymouth =
+    {
+      enable = true;
+
+    }
+    // (
+      # if device.presentation then
+      if true then
+        {
+          theme = "target";
+          themePackages = with pkgs; [
+            # By default we would install all themes
+            (adi1090x-plymouth-themes.override {
+              selected_themes = [ "target" ];
+            })
+          ];
+        }
+      else
+        {
+          themePackages = [ pkgs.mikuboot ];
+          theme = "mikuboot";
+        }
+    );
+
   boot.loader.grub2-theme = {
     enable = true;
     theme = "stylish";
--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
     ];
   };
   inputs = {
+    mikuboot.url = "gitlab:evysgarden/mikuboot";
     nixvim = {
       url = "github:nix-community/nixvim";
       # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
