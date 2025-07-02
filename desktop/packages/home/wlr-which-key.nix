fb1118c278336d90a1f6387ce10ac0a5fd4509ff
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-07-03 01:43:05 +0900

    New media controller keybind

--- desktop/packages/home/wlr-which-key.nix
+++ desktop/packages/home/wlr-which-key.nix
@@ -20,8 +20,11 @@ in
 {
   home.packages = [
     wlrWhichKey
+    pkgs.xclip
   ];
 
   xdg.configFile."wlr-which-key/config.yaml".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Sync/LinuxConfig/wlr-which-key/config.yaml";
+  xdg.configFile."wlr-which-key/media.yaml".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Sync/LinuxConfig/wlr-which-key/media.yaml";
 }
