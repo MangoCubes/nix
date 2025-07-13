76b3212ceaad87b56f6151e3ed9932ddd48d5390
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-07-13 16:02:38 +0900

    Add killclick

--- desktop/packages/home/niri.nix
+++ desktop/packages/home/niri.nix
@@ -7,16 +7,17 @@
 }:
 let
   configFile = if hostname == "work" then "config-work.kdl" else "config.kdl";
+  killclick = pkgs.writeShellScriptBin "killclick" ''kill -9 $(niri msg pick-window | grep PID | tail -n 1 | awk '{print $NF}')'';
 in
-
 {
-  home.packages = (
-    with pkgs;
+  home.packages =
     [
+      killclick
+    ]
+    ++ (with pkgs; [
       playerctl
       niri
-    ]
-  );
+    ]);
   xdg.configFile."niri/config.kdl".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Sync/LinuxConfig/niri/${configFile}";
 }
