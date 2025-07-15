8a072dd3ba3fde78c45bf2bca35f0c7387f3fef1
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-07-15 14:34:21 +0900

    Automount nextcloud

--- desktop/packages/home/nextcloud.nix
+++ desktop/packages/home/nextcloud.nix
@@ -1,7 +1,28 @@
-{ unstable, ... }:
+{
+  pkgs,
+  unstable,
+  inputs,
+  lib,
+  ...
+}:
 {
   # services.nextcloud-client.enable = true;
   # Nextcloud client will be started manually by niri
-  # systemd.user.services.nextcloud-client.Install.WantedBy = lib.mkForce [ ];
   home.packages = [ unstable.nextcloud-client ];
+  imports = [ inputs.secrets.hm.nextcloud ];
+  systemd.user.services = {
+    rclone-cloud = {
+      Unit = {
+        Description = "Mount Nextcloud drive automatically";
+      };
+      Service = {
+        Type = "notify";
+        ExecStartPre = "/run/current-system/sw/bin/mkdir -p %h/Mounts/Cloud";
+        ExecStart = "${pkgs.rclone}/bin/rclone --config=%h/.config/sops-nix/secrets/rclone-cloud --vfs-cache-mode writes mount \"cloud:\" %h/Mounts/Cloud --allow-other";
+        Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+        ExecStop = "/bin/fusermount -u %h/Mounts/Cloud";
+      };
+      Install.WantedBy = lib.mkForce [ "default.target" ];
+    };
+  };
 }
