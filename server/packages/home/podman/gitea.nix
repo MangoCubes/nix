08d35541cccfc1aefa1cebb2e2be39e07672a5b9
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-07-26 22:34:00 +0900

    use secrets to manage env variables

--- server/packages/home/podman/gitea.nix
+++ server/packages/home/podman/gitea.nix
@@ -15,16 +15,11 @@
       name = "gitea";
       volumes = [
         "${config.home.homeDirectory}/.podman/gitea:/data"
-        "/etc/timezone:/etc/timezone:ro"
         "/etc/localtime:/etc/localtime:ro"
       ];
       environment = {
         "USER_UID" = "1000";
         "USER_GID" = "1000";
-        "GITEA__database__DB_TYPE" = "mysql";
-        "GITEA__database__HOST" = "mariadb:3306";
-        "GITEA__database__NAME" = "giteadb";
-        "GITEA__database__USER" = "gitea";
         "GITEA__repository__DEFAULT_BRANCH" = "master";
       };
       domain = [
