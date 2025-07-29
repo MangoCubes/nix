2b8f7bb58074371abe622c1e2dd54a89e33a185e
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-07-29 14:18:40 +0900

    Re-adding filtered file

---
+++ common/users.nix
@@ -0,0 +1,23 @@
+{ username, ... }:
+
+{
+  nix.settings.trusted-users = [ "@wheel" ];
+  users.users = {
+    "${username}" = {
+      # Stop killing my fucking containers pls
+      linger = true;
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "shared"
+      ]; # Enable ‘sudo’ for the user.
+      # Other stuffs are in secrets
+    };
+    test = {
+      isNormalUser = true;
+      extraGroups = [ "shared" ];
+    };
+  };
+
+  users.groups.shared = { };
+}
