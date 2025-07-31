0c1bf297d1dffc442d8e7213e57df080c6dd5cfd
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-07-31 15:51:41 +0900

    Testing qutebrowser

---
+++ desktop/packages/home/qute.nix
@@ -0,0 +1,12 @@
+{ pkgs, config, ... }:
+let
+  qbtemp = pkgs.writeShellScriptBin "qbtemp" ''qutebrowser --temp-basedir -C ${config.home.homeDirectory}/.config/qutebrowser/config.py'';
+in
+{
+  home.packages = [
+    pkgs.qutebrowser
+    qbtemp
+  ];
+  xdg.configFile."qutebrowser/config.py".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Sync/LinuxConfig/qute/config.py";
+}
