42236cdcd20da3ddb3de4897bb27dd0ba32aad39
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-08-10 21:33:57 +0900

    Quick niri tricks

--- desktop/packages/home/wlr-which-key/niri.nix
+++ desktop/packages/home/wlr-which-key/niri.nix
@@ -14,7 +14,17 @@ let
     {
       key = "t";
       desc = "Tabbed Display";
-      cmd = ''toggle-column-tabbed-display'';
+      cmd = ''niri msg action toggle-column-tabbed-display'';
+    }
+    {
+      key = "s";
+      desc = "Split workspace";
+      cmd = ''niri msg action set-column-width 50% && niri msg action focus-column-right && niri msg action set-column-width 50% && niri msg action focus-column-left'';
+    }
+    {
+      key = "S";
+      desc = "Revert split workspace";
+      cmd = ''niri msg action set-column-width 90% && niri msg action focus-column-right && niri msg action set-column-width 90% && niri msg action focus-column-left'';
     }
   ];
 in
