21154018e98981e9a4615232ee31c0d856e4198b
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-08-21 01:51:40 +0900

    Fix collabora issues

--- server/packages/home/podman/collabora.nix
+++ server/packages/home/podman/collabora.nix
@@ -4,6 +4,23 @@
       dependsOn = [ "cloud" ];
       image = "collabora/code:latest";
       name = "collabora";
+      environment = {
+        "domain" = "cloud.skew.ch";
+        "VIRTUAL_PROTO" = "http";
+        "VIRTUAL_PORT" = "9980";
+        "VIRTUAL_HOST" = "office.skew.ch";
+        # Remember to put quotes around the parameters
+        # Notice how I put '' around "s
+        "extra_params" = ''"--o:ssl.enable=false --o:ssl.termination=true"'';
+      };
+      domain = [
+        {
+          routerName = "collabora";
+          url = "office.skew.ch";
+          type = 1;
+          port = 9980;
+        }
+      ];
     })
   ];
 }
