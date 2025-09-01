d6d26788da7a08cfb285049e9ad71f825ea97284
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-09-02 00:12:07 +0100

    Add restic

---
+++ common/packages/restic.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  username,
+  inputs,
+  ...
+}:
+let
+  user = "restic";
+in
+{
+  imports = [ inputs.secrets.server-main.restic ];
+  users.users."${user}".isNormalUser = true;
+  services.restic.backups = {
+    backblaze = {
+      inherit user;
+      initialize = true;
+      package = pkgs.writeShellScriptBin "restic" ''
+        export B2_ACCOUNT_ID=$(<"/run/secrets/restic/account-id")
+        export B2_ACCOUNT_KEY=$(<"/run/secrets/restic/account-key")
+        exec /run/wrappers/bin/restic "$@"
+      '';
+      repositoryFile = "/run/secrets/restic/repo";
+      passwordFile = "/run/secrets/restic/key";
+      timerConfig = {
+        OnUnitActiveSec = "1h";
+      };
+
+      pruneOpts = [
+        "--keep-weekly 20"
+        "--keep-yearly 50"
+      ];
+    };
+  };
+  security.wrappers.restic = {
+    source = "${pkgs.restic.out}/bin/restic";
+    owner = user;
+    group = "users";
+    permissions = "u=rwx,g=,o=";
+    capabilities = "cap_dac_read_search=+ep";
+  };
+  home-manager.users."${username}" =
+    { pkgs, ... }:
+    {
+      home.packages = [ pkgs.restic ];
+    };
+
+}
--- server/server-main/configuration.nix
+++ server/server-main/configuration.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     inputs.secrets.hw.image
+    ../../common/packages/restic.nix
     ../../common/packages/rclone-mega.nix
     ../packages/traefik.nix
     ../packages/podman/mitmproxy-wg.nix
