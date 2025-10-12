aafd646497c34c2713aeb6345ed916fbee2bfb6f
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-10-13 00:24:13 +0100

    Add plex documentation

--- server/packages/home/podman/plex.nix
+++ server/packages/home/podman/plex.nix
@@ -1,7 +1,13 @@
 {
   config,
+  inputs,
   ...
 }:
+# Plex how to claim a server
+# 1. Go to /config/Library/Application Support/Plex Media Server/Preferences.xml and get ProcessedMachineIdentifier value
+# 2. Go to https://account.plex.tv/claim and get your claim token
+# 3. Run the following command:
+#    curl -X POST -s -H "X-Plex-Client-Identifier: {processed_machine_identifier}" "https://plex.tv/api/claim/exchange?token={claim_token}"
 {
   imports = [
     ((import ../../../../lib/podman.nix) {
@@ -9,7 +15,7 @@
       image = "lscr.io/linuxserver/plex:latest";
       name = "plex";
       volumes = [
-        "${config.home.homeDirectory}/Mount/media/Library/Anime:/media/Anime"
+        "${config.home.homeDirectory}/Mounts/media/Library/Anime:/media/Anime"
         "${config.home.homeDirectory}/Mounts/media/Library/Music:/media/Music"
         "${config.home.homeDirectory}/.podman/plex:/config"
       ];
@@ -27,7 +33,6 @@
         "TZ" = "Etc/UTC";
         "VERSION" = "docker";
       };
-      environmentFile = [ ''${config.home.homeDirectory}/.config/sops-nix/secrets/plex'' ];
     })
   ];
 }
