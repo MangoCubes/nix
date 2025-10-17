250929b1c58ff0e231ec735ce4da03db4dec8bc1
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-10-18 00:00:58 +0100

    Specifying each files individually

--- server/packages/podman/mitmproxy-wg.nix
+++ server/packages/podman/mitmproxy-wg.nix
@@ -9,6 +9,7 @@
       config,
       inputs,
       pkgs,
+      lib,
       ...
     }:
     {
@@ -28,9 +29,19 @@
           ];
           ports = [ "51820:51820/udp" ];
           entrypoint = "mitmweb --web-host 0.0.0.0 --set confdir=/etc/mitm --set 'web_password=$argon2i$v=19$m=4096,t=3,p=1$c2FsdEl0V2l0aFNhbHQ$jJHYL8FmjFuaY6nOHa5sCJT6qlk2OPWCg/feeJ3rWk0' --set mode=wireguard";
-          volumes = [
-            "${config.home.homeDirectory}/.config/sops-nix/secrets/mitm:/etc/mitm"
-          ];
+          volumes =
+            let
+              files = inputs.secrets.res;
+            in
+            [
+              "${files}/keys/mitm/mitm.pem:/etc/mitm/mitmproxy-ca-cert.cer"
+              "${files}/keys/mitm/mitm.p12:/etc/mitm/mitmproxy-ca-cert.p12"
+              "${files}/keys/mitm/mitm.pem:/etc/mitm/mitmproxy-ca-cert.pem"
+              "${config.home.homeDirectory}/.config/sops-nix/secrets/mitm/mitmproxy-ca.p12:/etc/mitm/mitmproxy-ca.p12"
+              "${config.home.homeDirectory}/.config/sops-nix/secrets/mitm/mitmproxy-ca.pem:/etc/mitm/mitmproxy-ca.pem"
+              "${config.home.homeDirectory}/.config/sops-nix/secrets/mitm/mitmproxy-dhparam.pem:/etc/mitm/mitmproxy-dhparam.pem"
+              "${config.home.homeDirectory}/.config/sops-nix/secrets/mitm/wireguard.conf:/etc/mitm/wireguard.conf"
+            ];
         })
       ];
     };
