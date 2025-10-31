f6966d8e120999e8bdbc4d7fc1a3ed2c3cecfeab
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-10-31 00:35:54 +0000

    Add install-bootloader flags

--- common/packages/home/scripts/rebuild.sh
+++ common/packages/home/scripts/rebuild.sh
@@ -13,6 +13,10 @@ update_all=false
 # Reboot after successful rebuild
 reboot=false
 
+# Install bootloader
+# Without it, computer will not be able to boot into newer builds on reboot, and make it look like the computer is resetting every reboot
+bootloader=false
+
 if [[ $1 == *"s"* ]]; then
     rebuild_secrets=true
 fi
@@ -35,12 +39,17 @@ if [[ $1 == *"r"* ]]; then
     reboot=true
 fi
 
+if [[ $1 == *"b"* ]]; then
+    bootloader=true
+fi
+
 # Print the results
 echo "Rebuild secrets: $rebuild_secrets"
 echo "Presentation mode: $presentation"
 echo "Update unstable: $update_unstable"
 echo "Update all: $update_all"
 echo "Reboot after successful rebuild: $reboot"
+echo "Install bootloader: $bootloader"
 
 if [ "$rebuild_secrets" = true ]; then
 	sudo nix flake update secrets --flake $HOME/Sync/NixConfig
@@ -60,7 +69,13 @@ if [ "$update_all" = true ]; then
 	nix flake update --flake path://$HOME/Sync/NixConfig
 fi
 
-sudo nixos-rebuild --flake path://$HOME/Sync/NixConfig#$device_name switch
+if [ "$bootloader" = true ]; then
+	sudo nixos-rebuild --flake path://$HOME/Sync/NixConfig#$device_name switch --install-bootloader
+else 
+	sudo nixos-rebuild --flake path://$HOME/Sync/NixConfig#$device_name switch
+fi
+
+
 
 if [ "$reboot" = true ]; then
 	reboot
