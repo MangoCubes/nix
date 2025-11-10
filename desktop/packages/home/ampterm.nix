de35c6583e7a9d931baf4e00a00d2dab92cdebb4
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-11-10 18:06:41 +0000

    Update ampache config

--- desktop/packages/home/ampterm.nix
+++ desktop/packages/home/ampterm.nix
@@ -3,5 +3,19 @@
   imports = [ inputs.ampterm.homeManager.default ];
   programs.ampterm = {
     enable = true;
+    settings = {
+      keybindings = {
+        Common = {
+          "<Ctrl-w><q>" = "Quit";
+        };
+      };
+      use_legacy_auth = true;
+      auto_focus = true;
+      auth = {
+        url = "echo https://music.skew.ch";
+        username = "echo admin";
+        password = "secret-tool lookup Path '/Ampache'";
+      };
+    };
   };
 }
