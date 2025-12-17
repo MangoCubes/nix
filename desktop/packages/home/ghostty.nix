7769a6d54562f47b931471acfb511017d5294d08
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-12-17 01:34:01 +0000

    Using ghostty

--- desktop/packages/home/ghostty.nix
+++ desktop/packages/home/ghostty.nix
@@ -1,4 +1,33 @@
-{ unstable, ... }:
+{ unstable, pkgs, ... }:
+let
+  term = pkgs.writeShellScriptBin "term" ''ghostty "$@"'';
+  detached = pkgs.writeShellScriptBin "t" ''d ghostty bash'';
+  dt = pkgs.writeShellScriptBin "dt" ''d ghostty bash -c "$*"'';
+in
 {
-  home.packages = [ unstable.ghostty ];
+  programs.bash.shellAliases = {
+    fastfetch = "fastfetch --logo ~/.config/configMedia/logo/nix.png --logo-type kitty-direct --logo-width 30 --logo-height 14";
+  };
+  home.packages = [
+    detached
+    term
+    dt
+  ];
+
+  programs.ghostty = {
+    enable = true;
+    package = unstable.ghostty;
+    settings = {
+      font-family = "FiraCode Nerd Font";
+      custom-shader-animation = true;
+      custom-shader = [
+        "${./ghostty/blaze.glsl}"
+        "${./ghostty/trail.glsl}"
+      ];
+      background-opacity = 0.625;
+      background-blur = true;
+      background = "#000000";
+      font-size = 10;
+    };
+  };
 }
