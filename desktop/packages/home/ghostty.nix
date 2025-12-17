bc93b08da1450687438e6d82430fe3530232a240
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-12-17 01:52:49 +0000

    Remove ghostty temporarily

--- desktop/packages/home/ghostty.nix
+++ desktop/packages/home/ghostty.nix
@@ -1,19 +1,5 @@
-{ unstable, pkgs, ... }:
-let
-  term = pkgs.writeShellScriptBin "term" ''ghostty "$@"'';
-  detached = pkgs.writeShellScriptBin "t" ''d ghostty bash'';
-  dt = pkgs.writeShellScriptBin "dt" ''d ghostty bash -c "$*"'';
-in
+{ unstable, ... }:
 {
-  programs.bash.shellAliases = {
-    fastfetch = "fastfetch --logo ~/.config/configMedia/logo/nix.png --logo-type kitty-direct --logo-width 30 --logo-height 14";
-  };
-  home.packages = [
-    detached
-    term
-    dt
-  ];
-
   programs.ghostty = {
     enable = true;
     package = unstable.ghostty;
