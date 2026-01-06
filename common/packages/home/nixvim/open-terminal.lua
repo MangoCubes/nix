aac9a22a1513162bce9323cfdb7004dde9e8805c
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-01-06 00:04:23 +0000

    Fix missing quote

--- common/packages/home/nixvim/open-terminal.lua
+++ common/packages/home/nixvim/open-terminal.lua
@@ -5,7 +5,7 @@ function(state)
 
 	local lastSlashIndex = path:match("^(.*)/")
 
-	local command = 'ghostty --working-directory="' .. lastSlashIndex
+	local command = 'ghostty --working-directory="' .. lastSlashIndex .. '"'
 
 	vim.notify("Opening terminal in " .. lastSlashIndex .. "!")
 	os.execute("(" .. command .. " > /dev/null 2>&1 &)")
