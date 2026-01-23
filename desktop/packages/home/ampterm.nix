e6bfcd901164e95af57033e68140961af56b68e9
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-01-23 19:15:38 +0000

    Update ampterm options

--- desktop/packages/home/ampterm.nix
+++ desktop/packages/home/ampterm.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.ampterm.homeManager.default ];
   programs.ampterm = {
     enable = true;
-    extraOptions = {
+    settings = {
       auth = {
         url = "echo https://music.skew.ch";
         username = "echo admin";
@@ -15,6 +15,34 @@
         lyrics.enable = true;
         bpmtoy.enable = true;
         cover_art.enable = true;
+      };
+      local = {
+        select_playlist_popup = {
+          "<d>" = {
+            SelectID = {
+              id = "800000014";
+              name = "Duplicate";
+            };
+          };
+          "<c>" = {
+            SelectID = {
+              id = "800000016";
+              name = "Too Calm";
+            };
+          };
+          "<s>" = {
+            SelectID = {
+              id = "800000009";
+              name = "Short";
+            };
+          };
+          "<n>" = {
+            SelectID = {
+              id = "800000015";
+              name = "Not My Style";
+            };
+          };
+        };
       };
     };
   };
