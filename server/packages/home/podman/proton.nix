b8b1400aeca845854d6355bb4f22d207faca8f66
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-02-17 00:08:58 +0900

    Re-add country randomisation

--- server/packages/home/podman/proton.nix
+++ server/packages/home/podman/proton.nix
@@ -21,11 +21,10 @@
         "NET_RAW"
       ];
       devices = [ "/dev/net/tun:/dev/net/tun" ];
-      # entrypoint = ''
-      #   # export Country=$(/gluetun-entrypoint format-servers -protonvpn -format json | grep country | uniq | shuf | head -n 1 | sed -nE 's/.+"country": "(.+)".+/\1/p');
-      #   export Country="Netherlands";
-      #   /gluetun-entrypoint
-      # '';
+      entrypoint = ''
+        export Country=$(/gluetun-entrypoint format-servers -protonvpn -format json | grep country | uniq | shuf | head -n 1 | sed -nE 's/.+"country": "(.+)".+/\1/p');
+        /gluetun-entrypoint
+      '';
       environmentFile = [ "${config.home.homeDirectory}/.config/sops-nix/secrets/gluetun/${name}" ];
     }))
   ];
