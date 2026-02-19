a49c196dcfdd8e931407252c87feb3469823cc66
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-02-19 21:44:30 +0900

    Use netbird client

--- common/packages/netbird.nix
+++ common/packages/netbird.nix
@@ -1,8 +1,26 @@
+{ inputs, hostname, ... }:
 {
+  # environment.sessionVariables = {
+  #   QT_QPA_PLATFORM = "wayland";
+  # };
+  imports = [
+    (inputs.secrets.networks.netbird { inherit hostname; })
+  ];
   services.netbird = {
-    enable = true;
-    ui.enable = true;
     clients = {
+      personal = {
+        login = {
+          enable = true;
+          setupKeyFile = "/run/secrets/netbird";
+        };
+        ui.enable = true;
+        port = 51820;
+        environment = {
+          NB_CONFIG = "/var/lib/netbird-personal/config.json";
+          NB_MANAGEMENT_URL = "https://vpn.skew.ch";
+          NB_ADMIN_URL = "https://vpn.skew.ch";
+        };
+      };
     };
   };
 }
