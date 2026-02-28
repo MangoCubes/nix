e9b165fecb222250f75be408fad5988e86510418
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-03-01 01:11:06 +0900

    Add temp directory creating script

--- common/packages/home/scripts.nix
+++ common/packages/home/scripts.nix
@@ -6,11 +6,16 @@ let
   detached = pkgs.writeShellScriptBin "d" ''
     ("$@" > /dev/null 2>&1 &)
   '';
+  temp = pkgs.writeShellScriptBin "temp" ''
+    tempdir=$(mktemp -d "${"TMPDIR:-/tmp/"}$(basename $0).XXXXXXXXXXXX")
+    cd $tempdir
+  '';
   rebuild = pkgs.writeShellScriptBin "rebuild" (builtins.readFile ./scripts/rebuild.sh);
 in
 {
   home.packages = [
     detached
     rebuild
+    temp
   ];
 }
