2bb1da8100ae06eefe85b171fe2392fd70c9df10
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-03-27 10:17:30 +0900

    Add back adb

--- desktop/packages/android.nix
+++ desktop/packages/android.nix
@@ -26,25 +26,13 @@
       '';
     in
     {
-      xdg.desktopEntries.android-studio = {
-        comment = "The official Android IDE";
-        icon = "android-studio";
-        name = "Android Studio (stable channel)";
-        terminal = false;
-        categories = [
-          "Development"
-          "IDE"
-        ];
-        exec = ''env _JAVA_AWT_WM_NONREPARENTING=1 _JAVA_AWT_WM_NONREPARENTING=1 AWT_TOOLKIT=MToolkit GDK_BACKEND=x11 LANG=en_US.UTF-8 QT_QPA_PLATFORM=xcb android-studio'';
-      };
-
       home.packages = lib.mkMerge [
         (lib.mkIf (androidStudio) [ unfreeUnstable.android-studio ])
+        (lib.mkIf (heimdall) [ pkgs.heimdall-gui ])
         [
           pkgs.scrcpy
           mount-android
         ]
-        (lib.mkIf (heimdall) [ pkgs.heimdall-gui ])
       ];
     };
   programs.adb.enable = true;
