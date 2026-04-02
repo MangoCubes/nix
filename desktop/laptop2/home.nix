157294990fd8998e3ea61df52c43babaf53f2277
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-04-02 18:24:26 +0900

    Add back ghidra

--- desktop/laptop2/home.nix
+++ desktop/laptop2/home.nix
@@ -5,11 +5,10 @@
     {
       imports = [
         ../packages/home/windows.nix
+        ../packages/home/ghidra.nix
       ];
       home.packages = [
         unfreeUnstable.osu-lazer-bin
-        unstable.krita
-        unstable.gdb
       ];
     };
 }
