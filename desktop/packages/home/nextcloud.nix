8a8b6530cf5c316c11bf0e4da54dfb0df4dc7b48
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-05-01 18:21:28 +0900

    Declarative Nextcloud sync

--- desktop/packages/home/nextcloud.nix
+++ desktop/packages/home/nextcloud.nix
@@ -1,28 +1,82 @@
 {
   pkgs,
-  unstable,
+  config,
   inputs,
   lib,
   ...
 }:
+let
+  syncDirs = [
+    "Temp"
+    "Documents/Important"
+    "Documents/School/6-1"
+    "Documents/School/Research/Current"
+    "Documents/School/Research/Templates"
+    "Documents/School/TA"
+    "Documents/School/Work/2026"
+    "Projects/ampterm"
+    "Projects/dragevac"
+    "Projects/School"
+  ];
+  syncCmd =
+    dir:
+    "mkdir -p ${config.home.homeDirectory}/Nextcloud/${dir} && ${pkgs.nextcloud-client}/bin/nextcloudcmd -u $NC_USER -p $NC_PASSWORD -h --exclude ${./nextcloud/exclude.lst} --path /${dir} ${config.home.homeDirectory}/Nextcloud/${dir} https://cloud.skew.ch";
+  syncScript =
+    let
+      allCmds = (builtins.concatStringsSep "\n" (builtins.map syncCmd syncDirs));
+    in
+    pkgs.writeShellScriptBin "sync-script" ''
+      NC_USER=$(${pkgs.libsecret}/bin/secret-tool lookup Use Nextcloud_Username)
+      NC_PASSWORD=$(${pkgs.libsecret}/bin/secret-tool lookup Use Nextcloud_Password)
+      ${allCmds}
+    '';
+in
 {
-  # services.nextcloud-client.enable = true;
-  # Nextcloud client will be started manually by niri
-  home.packages = [ unstable.nextcloud-client ];
+  home.packages = [
+    syncScript
+  ];
   imports = [ inputs.secrets.hm.nextcloud ];
-  systemd.user.services = {
-    rclone-cloud = {
-      Unit = {
-        Description = "Mount Nextcloud drive automatically";
+  systemd.user = {
+    services = {
+      rclone-cloud = {
+        Unit = {
+          Description = "Mount Nextcloud drive automatically";
+        };
+        Service = {
+          Type = "notify";
+          ExecStartPre = "/run/current-system/sw/bin/mkdir -p %h/Mounts/Cloud";
+          ExecStart = "${pkgs.rclone}/bin/rclone --config=%h/.config/sops-nix/secrets/rclone-cloud --vfs-cache-mode full mount \"cloud:\" %h/Mounts/Cloud";
+          Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+          ExecStop = "/bin/fusermount -u %h/Mounts/Cloud";
+        };
+        Install.WantedBy = lib.mkForce [ "default.target" ];
       };
-      Service = {
-        Type = "notify";
-        ExecStartPre = "/run/current-system/sw/bin/mkdir -p %h/Mounts/Cloud";
-        ExecStart = "${pkgs.rclone}/bin/rclone --config=%h/.config/sops-nix/secrets/rclone-cloud --vfs-cache-mode full mount \"cloud:\" %h/Mounts/Cloud";
-        Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
-        ExecStop = "/bin/fusermount -u %h/Mounts/Cloud";
-      };
-      Install.WantedBy = lib.mkForce [ "default.target" ];
+      # nextcloud-autosync = {
+      #   Unit = {
+      #     Description = "Auto sync Nextcloud";
+      #     After = "network-online.target";
+      #   };
+      #   Service = {
+      #     Type = "simple";
+      #     ExecStart = "${syncScript}/bin/sync-cmd";
+      #     TimeoutStopSec = "180";
+      #     KillMode = "process";
+      #     KillSignal = "SIGINT";
+      #   };
+      #   Install.WantedBy = [ "multi-user.target" ];
+      # };
     };
+    timers.nextcloud-autosync = {
+      Unit.Description = "Automatic sync files with Nextcloud when booted up after 5 minutes then rerun every 60 minutes";
+      Timer.OnBootSec = "5min";
+      Timer.OnUnitActiveSec = "60min";
+      Install.WantedBy = [
+        "multi-user.target"
+        "timers.target"
+      ];
+    };
+    startServices = true;
+  };
+  systemd.user.services = {
   };
 }
