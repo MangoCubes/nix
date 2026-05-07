e64de7eea1603e6486f0d5efea0b0d7007cc0a83
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-05-07 18:21:29 +0900

    Add new directories to sync

--- desktop/packages/home/nextcloud.nix
+++ desktop/packages/home/nextcloud.nix
@@ -14,6 +14,7 @@ let
     "Documents/School/Research/Templates"
     "Documents/School/TA"
     "Documents/School/Work/2026"
+    "Documents/SelfStudy/Japanese"
     "Projects/ampterm"
     "Projects/dragevac"
     "Projects/School"
