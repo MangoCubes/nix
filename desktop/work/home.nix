e89236345731ddb9046e854e39b4566c32d68253
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-05-18 16:43:23 +0900

    Remove unused stuff

--- desktop/work/home.nix
+++ desktop/work/home.nix
@@ -1,23 +1,14 @@
 { username, ... }:
 {
   home-manager.users."${username}" =
-    {
-      pkgs,
-      unstable,
-      unfreeUnstable,
-      unfree,
-      inputs,
-      ...
-    }:
+    { unfreeUnstable, ... }:
     {
       imports = [
-        inputs.secrets.hm.programs
         ../packages/home/windows.nix
         ../packages/home/ghidra.nix
       ];
       home.packages = [
         unfreeUnstable.osu-lazer-bin
-        unfree.beeper
       ];
     };
 }
