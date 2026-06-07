be22169a2e8c65e41c2f7f39deafd7ee1be26864
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-06-08 01:59:36 +0900

    Using graphical emacs

--- desktop/packages/home/emacs.nix
+++ desktop/packages/home/emacs.nix
@@ -16,10 +16,10 @@ let
   init = "~/Sync/EmacsConfig/init.el";
 
   loademacs = pkgs.writeShellScriptBin "loademacs" "${envs} emacs -q --daemon --load ${init}";
-  emacs-org = pkgs.writeShellScriptBin "emacs-org" ''tc emacsclient -nw --eval '(find-file "${config.home.homeDirectory}/Sync/Notes/Org/Main.org")' '';
+  emacs-org = pkgs.writeShellScriptBin "emacs-org" ''emacsclient -c --eval '(find-file "${config.home.homeDirectory}/Sync/Notes/Org/Main.org")' '';
   emacs-web = pkgs.writeShellScriptBin "emacs-web" ''emacsclient -c --eval '(find-file "${config.home.homeDirectory}/Sync/Website/src/org/index.org")' '';
   emacs-mail = pkgs.writeShellScriptBin "emacs-mail" ''emacsclient -c -e '(notmuch-search "tag:inbox")' '';
-  emacs-daily = pkgs.writeShellScriptBin "emacs-daily" "tc emacsclient -nw -e '(org-roam-dailies-goto-today)' ";
+  emacs-daily = pkgs.writeShellScriptBin "emacs-daily" "emacsclient -c -e '(org-roam-dailies-goto-today)' ";
   reload-emacs = pkgs.writeShellScriptBin "er" "emacsclient -e '(kill-emacs)'; loademacs";
   reload-emacs-fg = pkgs.writeShellScriptBin "ef" "${envs} emacs -q --fg-daemon --load ${init}";
   tex = (
