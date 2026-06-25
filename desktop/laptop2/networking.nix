40f21ca5f7ad1929ebc8b0cb7c550dc7c5d43c2e
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-06-25 13:55:24 +0900

    Closing that port now

--- desktop/laptop2/networking.nix
+++ desktop/laptop2/networking.nix
@@ -3,12 +3,6 @@
   imports = [
     inputs.secrets.networks.wifi
   ];
-  networking.firewall = {
-    enable = true;
-    allowedUDPPorts = [
-      45919
-    ];
-  };
   services.avahi = {
     enable = true;
   };
