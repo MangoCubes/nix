badc73faf445d449a389cc5837f5ef3844ed2b6b
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-07-27 17:26:07 +0900

    Remove vm

--- server/server-home/traefik.nix
+++ server/server-home/traefik.nix
@@ -7,28 +7,7 @@
   ...
 }:
 let
-  traefikFile = (
-    lib.recursiveUpdate {
-      dynamic = {
-        http = {
-          serversTransports.insecure.insecureSkipVerify = true;
-          routers.proxmox = {
-            rule = "Host(`vm.int`)";
-            service = "proxmox";
-            entryPoints = [ "websecure" ];
-            tls.certResolver = "localca";
-          };
-          services.proxmox.loadBalancer = {
-            serversTransport = "insecure";
-            servers = [
-              { url = "https://host.containers.internal:8006"; }
-            ];
-          };
-        };
-      };
-      static = { };
-    } inputs.secrets.server-home.traefik
-  );
+  traefikFile = (inputs.secrets.server-home.traefik);
 in
 ((import ../../common/packages/podman/traefik.nix) {
   inherit
