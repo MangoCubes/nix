ac7732fc66dfda46a0ab1f15e5efa04337b281a8
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-08-21 16:06:02 +0900

    Update ports to use

--- desktop/laptop2/networking.nix
+++ desktop/laptop2/networking.nix
@@ -3,4 +3,17 @@
   imports = [
     inputs.secrets.networks.wifi
   ];
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      # Port for letting desktops to connect to me
+      # 34669
+      # 45371
+      8770
+    ];
+    allowedUDPPorts = [
+      # Port for letting desktops to connect to me
+      5353
+    ];
+  };
 }
