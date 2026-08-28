3bf67e979054f4d8f784ed64e43804e2f535f59b
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-08-28 10:38:52 +0900

    Trying out OWL

---
+++ desktop/packages/owl.nix
@@ -0,0 +1,88 @@
+{ pkgs, config, ... }:
+let
+  owl = pkgs.callPackage (
+    {
+      stdenv,
+      fetchFromGitHub,
+      cmake,
+      pkg-config,
+      libpcap,
+      libev,
+      libnl,
+    }:
+
+    stdenv.mkDerivation {
+      pname = "owl";
+      version = "unstable-master";
+
+      src = fetchFromGitHub {
+        owner = "seemoo-lab";
+        repo = "owl";
+        rev = "da255a70f221784c836d943dd3f243bc798f223b";
+        fetchSubmodules = true;
+        hash = "sha256-727xTLQ3a6a+yJWIyB33UNUI5wl4ZaO6bPkt+WNgYEM=";
+      };
+
+      nativeBuildInputs = [
+        cmake
+        pkg-config
+      ];
+      buildInputs = [
+        libpcap
+        libev
+        libnl
+      ];
+
+      cmakeFlags = [
+        "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+      ];
+
+      postPatch = ''
+        substituteInPlace CMakeLists.txt \
+          --replace-fail 'add_subdirectory(googletest)' "" \
+          --replace-fail 'add_subdirectory(tests)' ""
+      '';
+    }
+  ) { };
+in
+{
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+    allowedTCPPorts = [
+      # Port for letting desktops to connect to me
+      # 34669
+      # 45371
+      8770
+      41159
+    ];
+    allowedUDPPorts = [
+      # Port for letting desktops to connect to me
+      5353
+    ];
+    trustedInterfaces = [
+      "awdl0"
+    ];
+  };
+
+  systemd.services.open-wireless-link = {
+    description = "Open Wireless Link (AWDL Daemon)";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStartPre = [
+        "${pkgs.iw}/bin/iw dev ${config.custom.networking.primary} interface add mon0 type monitor"
+        "${pkgs.iproute2}/bin/ip link set mon0 up"
+      ];
+      ExecStart = "${owl}/bin/owl -i mon0 -N";
+      ExecStopPost = [
+        "${pkgs.iproute2}/bin/ip link set mon0 down"
+        "${pkgs.iw}/bin/iw dev mon0 del"
+      ];
+      Restart = "on-failure";
+      RestartSec = "5s";
+      CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_RAW";
+      AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_RAW";
+    };
+  };
+}
