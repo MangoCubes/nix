e0698031ebb8ec41d74d293359e447f8e923904f
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-09-02 16:36:38 +0900

    Use my own repo instead

--- desktop/packages/owl.nix
+++ desktop/packages/owl.nix
@@ -16,11 +16,11 @@ let
       version = "unstable-master";
 
       src = fetchFromGitHub {
-        owner = "seemoo-lab";
+        owner = "MangoCubes";
         repo = "owl";
-        rev = "da255a70f221784c836d943dd3f243bc798f223b";
+        rev = "0dcc85eeefea36736036ebe9481076932acee400";
         fetchSubmodules = true;
-        hash = "sha256-727xTLQ3a6a+yJWIyB33UNUI5wl4ZaO6bPkt+WNgYEM=";
+        hash = "sha256-gNSaR7kkEb1BzWN6tBNJj5Etq+KZ+dbj+MLE4GcYocI=";
       };
 
       nativeBuildInputs = [
@@ -36,55 +36,34 @@ let
       cmakeFlags = [
         "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
       ];
-
-      postPatch = ''
-        substituteInPlace CMakeLists.txt \
-          --replace-fail 'add_subdirectory(googletest)' "" \
-          --replace-fail 'add_subdirectory(tests)' ""
-      '';
     }
   ) { };
 in
 {
   networking.firewall = {
     enable = true;
-    allowPing = true;
-    allowedTCPPorts = [
-      # Port for letting desktops to connect to me
-      # 34669
-      # 45371
-      8770
-      41159
-    ];
-    allowedUDPPorts = [
-      # Port for letting desktops to connect to me
-      5353
-    ];
-    trustedInterfaces = [
-      "awdl0"
-    ];
+    trustedInterfaces = [ "awdl0" ];
   };
-
   environment.systemPackages = [ owl ];
-
-  systemd.services.open-wireless-link = {
-    description = "Open Wireless Link (AWDL Daemon)";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStartPre = [
-        "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} down"
-        "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} promisc on"
-      ];
-      ExecStart = "${owl}/bin/owl -i ${config.custom.networking.secondary}";
-      ExecStopPost = [
-        "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} promisc off"
-        "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} up"
-      ];
-      Restart = "on-failure";
-      RestartSec = "5s";
-      CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_RAW";
-      AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_RAW";
-    };
-  };
+  #
+  # systemd.services.open-wireless-link = {
+  #   description = "Open Wireless Link (AWDL Daemon)";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStartPre = [
+  #       "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} down"
+  #       "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} promisc on"
+  #     ];
+  #     ExecStart = "${owl}/bin/owl -i ${config.custom.networking.secondary}";
+  #     ExecStopPost = [
+  #       "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} promisc off"
+  #       "${pkgs.iproute2}/bin/ip link set ${config.custom.networking.secondary} up"
+  #     ];
+  #     Restart = "on-failure";
+  #     RestartSec = "5s";
+  #     CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_RAW";
+  #     AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_RAW";
+  #   };
+  # };
 }
